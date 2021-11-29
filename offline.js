eee96bfd266411782637e796bf5fbc26556f9bce
PopodoesSC <gabrielpeixoto@live.com>
2021-11-29 14:32:12 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1638206873,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/trashbottom-sheet0.png",
+		"images/trashtop-sheet0.png",
+		"images/fade.png",
+		"images/fundo.png",
+		"images/whatsappimage20211108at-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/point-sheet0.png",
+		"images/point-sheet1.png",
+		"images/point-sheet2.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"media/07-aquatic-ambiance.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
